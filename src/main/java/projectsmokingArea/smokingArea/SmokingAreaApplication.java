62965f54b617079fb1107dc90f1d889c090e9eb9
Minsoo Choi <lucas0209@yonsei.ac.kr>
2022-08-30 00:02:28 +0900

    commit

---
+++ src/main/java/projectsmokingArea/smokingArea/SmokingAreaApplication.java
@@ -0,0 +1,13 @@
+package projectsmokingArea.smokingArea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmokingAreaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmokingAreaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projectsmokingArea/smokingArea/SmokingAreaApplicationTests.java
@@ -0,0 +1,13 @@
+package projectsmokingArea.smokingArea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmokingAreaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
